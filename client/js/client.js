301789af16f28465842c6bd7bc435e1e25b9ab85
pekko1215 <pekko1215@yahoo.co.jp>
2017-10-06 18:36:21 +0900

    田頭現配列の呪い

--- client/js/client.js
+++ client/js/client.js
@@ -5,65 +5,65 @@ var height = 667;
 var stage = new PIXI.Container();
 
 // レンダラーを作る
-var renderer = new PIXI.WebGLRenderer(width, height, {backgroundColor: 0x000000});
+var renderer = new PIXI.WebGLRenderer(width, height, { backgroundColor: 0x000000 });
 
 // レンダラーのviewをDOMに追加する
 document.getElementById("pixiview").appendChild(renderer.view);
 
 
 //キーコード
-function keyboard(keyCode) {                                                         
-    var key     = {}                                                                 
-    key.code    = keyCode                                                            
-    key.isDown  = false                                                              
-    key.isUp    = true                                                               
-    key.press   = undefined                                                          
-    key.release = undefined                                                          
-    //The `downHandler`                                                              
-    key.downHandler = function(event) {                                              
-        if (event.keyCode === key.code) {    
-            //console.log(key);                                        
-            if (key.isUp && key.press) { key.press(); }                               
-            key.isDown = true 
-            key.isUp = false                                                         
-        }                                                                            
-        event.preventDefault()                                                       
-    }                                                                                
+function keyboard(keyCode) {
+    var key = {}
+    key.code = keyCode
+    key.isDown = false
+    key.isUp = true
+    key.press = undefined
+    key.release = undefined
+    //The `downHandler`
+    key.downHandler = function(event) {
+        if (event.keyCode === key.code) {
+            //console.log(key);
+            if (key.isUp && key.press) { key.press(); }
+            key.isDown = true
+            key.isUp = false
+        }
+        event.preventDefault()
+    }
     //The `upHandler`                                                                
-    key.upHandler = function(event) {                                                
-        if (event.keyCode === key.code) {                                            
-            if (key.isDown && key.release) { key.release() }                         
-            key.isDown = false                                                       
-            key.isUp = true                                                          
-        }                                                                            
-        event.preventDefault()                                                       
-    }                                                                                
+    key.upHandler = function(event) {
+        if (event.keyCode === key.code) {
+            if (key.isDown && key.release) { key.release() }
+            key.isDown = false
+            key.isUp = true
+        }
+        event.preventDefault()
+    }
     //Attach event listeners                                                         
-    window.addEventListener(                                                         
+    window.addEventListener(
         "keydown", key.downHandler.bind(key), false
-    )                                                                                
-    window.addEventListener(                                                         
-        "keyup", key.upHandler.bind(key), false                                      
-    )                                                                                
-    return key                                                                       
-}                                                                                    
-                                                                                     
+    )
+    window.addEventListener(
+        "keyup", key.upHandler.bind(key), false
+    )
+    return key
+}
+
 //var keyObject = keyboard(asciiKeyCodeNumber)
 
 //キーボード                                                                     
-var left  = keyboard(37)                                                         
-var up    = keyboard(38)                                                         
-var right = keyboard(39)                                                         
-var down  = keyboard(40) 
+var left = keyboard(37)
+var up = keyboard(38)
+var right = keyboard(39)
+var down = keyboard(40)
 
 var moveHor = 0;
 var moveVer = 0;
 
-function VecHor(vx) {                                                          
-    return function() {                                                          
-        moveHor = vx;                                                             
-    }                                                                           
-}                       
+function VecHor(vx) {
+    return function() {
+        moveHor = vx;
+    }
+}
 
 function VecVer(vy) {
     return function() {
@@ -71,15 +71,15 @@ function VecVer(vy) {
     }
 }
 
-right.press = VecHor(-5);                                                       
-left.press  = VecHor( 5);                                                       
-up.press    = VecVer( 5);                                                       
-down.press  = VecVer(-5);                                                       
-                                                                                    
-right.release = VecHor(0);                                                          
-left.release  = VecHor(0);                                                          
-up.release    = VecVer(0);                                                          
-down.release  = VecVer(0);
+right.press = VecHor(-5);
+left.press = VecHor(5);
+up.press = VecVer(5);
+down.press = VecVer(-5);
+
+right.release = VecHor(0);
+left.release = VecHor(0);
+up.release = VecVer(0);
+down.release = VecVer(0);
 
 
 // オブジェクトを作る
@@ -93,38 +93,41 @@ var Vertical = 30;
 var bord = [...Array(Horizontal).fill([...Array(Vertical).fill(0)])];
 //------------------
 
-var bordAxis = new PIXI.DisplayObjectContainer();     //ボードを動かすための基点
+var bordAxis = new PIXI.DisplayObjectContainer(); //ボードを動かすための基点
 
-var bordSquares = [...Array(Horizontal).fill([])];
+var bordSquares = [...Array(Horizontal).fill(0).map(()=>{return [...Array(Vertical)]})];
 var squareWidth = 40;
 var squareHeight = 40;
-bord.forEach(function(bordLine,index1){
-    bordLine.forEach(function(square,index2){
+bord.forEach(function(bordLine, index1) {
+    bordLine.forEach(function(square, index2) {
         //console.log(index1 + " , " + index2);
         bordSquares[index1][index2] = new PIXI.Graphics();
-        switch(square){
+        switch (square) {
             case 0:
                 //console.log(bordSquares[index1][index2]);
-                bordSquares[index1][index2].beginFill(0xffffff,1);
+                bordSquares[index1][index2].beginFill(0xffffff, 1);
                 bordSquares[index1][index2].tint = 0xccccccc;
-            break;
+                break;
             case 1:
-                bordSquares[index1][index2].beginFill(0xccffcc,1);
-            break;
+                bordSquares[index1][index2].beginFill(0xccffcc, 1);
+                break;
         }
-        bordSquares[index1][index2].lineStyle(2,0x000000);
-        bordSquares[index1][index2].drawRect(index1*squareWidth, index2*squareHeight, squareWidth, squareHeight);
+        bordSquares[index1][index2].lineStyle(2, 0x000000);
+        bordSquares[index1][index2].drawRect(0,0, squareWidth, squareHeight);
         bordSquares[index1][index2].endFill();
+        bordSquares[index1][index2].position.x = index1 * squareWidth;
+        bordSquares[index1][index2].position.y = index2 * squareHeight;
         bordAxis.addChild(bordSquares[index1][index2]);
     })
+console.log(bordSquares[0][0]["x"])
 })
 //----------------------------
 
 //--プレイヤーの生成-----------
 var PlayerPoint = new PIXI.Graphics();
 PlayerPoint.lineStyle(0);
-PlayerPoint.beginFill(0xccccff,1);
-PlayerPoint.drawCircle(width/2, height/2, 10);
+PlayerPoint.beginFill(0xccccff, 1);
+PlayerPoint.drawCircle(width / 2, height / 2, 10);
 PlayerPoint.endFill();
 
 
@@ -139,43 +142,43 @@ stage.addChild(PlayerPoint);
 animate();
 
 // アニメーション関数を定義する
-function animate(){
+// setInterval(()=>console.log(bordSquares[1][2].x),1000);
+function animate() {
 
     requestAnimationFrame(animate); // 次の描画タイミングでanimateを呼び出す
     //bordSquares[5][0].position.x += 1;
     bordAxis.position.x += moveHor;
     bordAxis.position.y += moveVer;
-    bord.forEach(function(bordLine, index1){
-        bord.forEach(function(square, index2){
-            switch(true){
-                case bordSquares[index1][index2].position.x >= (width/2) - squareWidth :
-                case bordSquares[index1][index2].position.x < width/2 :
-                case bordSquares[index1][index2].position.y >= height/2 :
-                case bordSquares[index1][index2].position.y < (height/2) - squareHeight :
-                   // console.log("にょ");
+
+    bord.forEach(function(bordLine, index1) {
+        bord.forEach(function(square, index2) {
+            switch (true) {
+                case (bordSquares[index1][index2].position.x >= (width / 2) - squareWidth) &&
+                (bordSquares[index1][index2].position.x < width / 2) &&
+                (bordSquares[index1][index2].position.y >= height / 2) &&
+                (bordSquares[index1][index2].position.y < (height / 2) - squareHeight):
+                    // console.log("にょ");
                     bord[index1][index2] = 1;
-                break;
-                default :
+                    break;
+                default:
                     bord[index1][index2] = 0;
             }
-            switch(bord[index1][index2]){
+            switch (bord[index1][index2]) {
                 case 0:
                     bordSquares[index1][index2].tint = 0x55cc55;
-                break;
+                    break;
                 case 1:
                     bordSquares[index1][index2].tint = 0xccffcc;
                     //console.log(bordSquares[index1][index2].tint)
-                break;
-                default :
+                    break;
+                default:
                     bordSquares[index1][index2].tint = 0x555555;
                     console.log("zoba")
             }
         })
     })
-    renderer.render(stage);   // 描画する
-    
+    renderer.render(stage); // 描画する
+
 }
 
 renderer.render(stage);
-
-
